cc277de923ae5aeff53c668b6ceb4f2c38218529
bobby siva sai ganesh <bobbysivasaiganesh567@gmail.com>
2026-08-13 23:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def video():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",10000)))
